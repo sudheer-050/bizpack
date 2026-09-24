1ed6493c1de17201e1965c223d74e296c50dcf14
Sudheer_050 <gsudheer56@gmail.com>
2026-09-24 10:15:24 -0500

    Enable interactive currency prompt in clean.py

--- examples/clean.py
+++ examples/clean.py
@@ -1,4 +1,4 @@
 import bizpack as bp
 print("Before cleaning:\n", bp.read_csv("dirty_data.csv", clean=False).head(10))
-clean_df = bp.clean_file("dirty_data.csv", "clean_data.csv", target_currency="INR")
+clean_df = bp.clean_file("dirty_data.csv", "clean_data.csv")
 print("\nAfter cleaning:\n", clean_df.head(10))
